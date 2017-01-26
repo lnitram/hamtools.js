77a130e1475590c26d969bd01d84cda8028576f1
Martin Leuschner <martinleuschner@googlemail.com>
2017-01-26 21:18:51 +0100

    convert frequency to wavelength

--- hamtools.js
+++ hamtools.js
@@ -1,7 +1,14 @@
 function Hamtools() {
+  var c = 299792458; // m/s
   this.hello = function() {
     return "Hello world!";
   }
+
+  this.f2l = function (frequency) {
+     return c/(frequency*1000000);
+  }
+
+
 };
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
--- test/test.js
+++ test/test.js
@@ -2,6 +2,13 @@ var assert = require('assert');
 var hamtools = require("../hamtools.js");
 
 describe('hamtools', function() {
+  describe('Frequency tests',  function() {
+    it("Test f to wavelength", function() {
+      var tools = new hamtools.Hamtools();
+      assert.ok(Math.abs(2.06753 - tools.f2l(145))<0.00001);
+    });
+  });
+
   describe('Locator tests', function() {
     it("Test hello world", function() {
       var tools =  new hamtools.Hamtools();
