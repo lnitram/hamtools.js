e6b0f4b8a17d8a9bf5199b25c43f02a38ab5b590
Martin Leuschner <martinleuschner@googlemail.com>
2017-01-26 21:34:09 +0100

    convert maidenhead locator to lon/lat

--- hamtools.js
+++ hamtools.js
@@ -3,7 +3,41 @@ function Hamtools() {
 
   this.f2l = function (frequency) {
      return c/(frequency*1000000);
-  }
+  };
+
+  this.locator2pos = function(locator) {
+    var d1 = "ABCDEFGHIJKLMNOPQR".split("");
+    var d2 = "ABCDEFGHIJKLMNOPQRSTUVWX".split("");
+    var len = locator.length;
+
+    if((len % 2 != 0) || (len > 6)) {
+      throw ("Invalid locator string");
+    }
+
+    var x1 = (locator.toUpperCase().charCodeAt(0)-65)*20 - 180;
+    var x2 = x1 + 20;
+    var y1 = (locator.toUpperCase().charCodeAt(1)-65)*10 - 90;
+    var y2 = y1 + 10;
+
+    if(len>= 4) {
+      x1 = x1 + (locator.charCodeAt(2)-48)*2;
+      x2 = x1 + 2;
+      y1 = y1 + locator.charCodeAt(3)-48;
+      y2 = y1 + 1;
+    }
+
+    if(len == 6) {		
+      x1 = x1 + (locator.toUpperCase().charCodeAt(4)-65)*2/24;
+      x2 = x1 + 2/24;
+      y1 = y1 + (locator.toUpperCase().charCodeAt(5)-65)*1/24;
+      y2 = y1 + 1/24;
+    }
+
+    return {north:y2, south:y1, west:x1, east:x2, center:{lon: x1 + (x2-x1)/2., lat: y1 + (y2-y1)/2.}}
+   
+
+
+  };
 
 
 };
--- test/test.js
+++ test/test.js
@@ -10,6 +10,15 @@ describe('hamtools', function() {
   });
 
   describe('Locator tests', function() {
-
+    it("Test locator", function() {
+      var tools = new hamtools.Hamtools();
+      var res = tools.locator2pos("JO43UN");
+      assert.ok(Math.abs(res.north - 53.58333) < 0.00001);
+      assert.ok(Math.abs(res.south - 53.54167) < 0.00001);
+      assert.ok(Math.abs(res.west  -  9.66667) < 0.00001);
+      assert.ok(Math.abs(res.east  -  9.75   ) < 0.00001);
+      assert.ok(Math.abs(res.center.lon - 9.70833) < 0.00001);
+      assert.ok(Math.abs(res.center.lat - 53.5625) < 0.00001);
+    });
   });
 });
